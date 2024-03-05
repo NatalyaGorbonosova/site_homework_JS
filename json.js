b2386ed4cd65bec8f2e3b66545ab9e99c8fd955b
NatalyaGorbonosova <111018572+NatalyaGorbonosova@users.noreply.github.com>
2024-03-05 09:44:42 +0300

    homework JS

---
+++ json.js
@@ -0,0 +1,96 @@
+const catalogData = `[
+    {
+        "id": 1,
+        "img": "img/catalog1.svg",
+        "title":"ELLERY X M'O CAPSULE",
+        "text":"Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "price":"$52.00"
+    },
+    {
+        "id": 2,
+        "img": "img/catalog2.svg",
+        "title":"ELLERY X M'O CAPSULE",
+        "text":"Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "price":"$52.00"  
+    },
+    {
+        "id": 3,
+        "img": "img/catalog3.svg",
+        "title":"MANGO  PEOPLE  T-SHIRT",
+        "text":"Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "price":"$300"
+    }, 
+    {
+        "id": 4,
+        "img": "img/catalog4.svg",
+        "title":"ELLERY X M'O CAPSULE",
+        "text":"Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "price":"$52.00"
+    },
+    {
+        "id": 5,
+        "img": "img/catalog5.svg",
+        "title":"ELLERY X M'O CAPSULE",
+        "text":"Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "price":"$52.00"
+    },
+    {
+        "id": 6,
+        "img": "img/catalog6.svg",
+        "title":"ELLERY X M'O CAPSULE",
+        "text":"Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "price":"$52.00"
+    }
+
+]`;
+
+const cartItemsData = `[
+    {
+        "id": 1,
+        "img": "img/catalog1.svg",
+        "title":"ELLERY X M'O CAPSULE",
+        "color": "Red",
+        "size": "XL",
+        "price":"$52.00"
+    },
+    {
+        "id": 2,
+        "img": "img/catalog2.svg",
+        "title":"ELLERY X M'O CAPSULE",
+        "color": "Red",
+        "size": "XL",
+        "price":"$52.00"
+    },
+    {
+        "id": 3,
+        "img": "img/catalog3.svg",
+        "title":"ELLERY X M'O CAPSULE",
+        "color": "Red",
+        "size": "XL",
+        "price":"$52.00"
+    },
+    {
+        "id": 4,
+        "img": "img/catalog4.svg",
+        "title":"ELLERY X M'O CAPSULE",
+        "color": "Black",
+        "size": "XL",
+        "price":"$52.00"
+    },
+    {
+        "id": 5,
+        "img": "img/catalog5.svg",
+        "title":"ELLERY X M'O CAPSULE",
+        "color": "Red",
+        "size": "XL",
+        "price":"$52.00"
+    },
+    {
+        "id": 6,
+        "img": "img/catalog6.svg",
+        "title":"ELLERY X M'O CAPSULE",
+        "color": "Black",
+        "size": "XL",
+        "price":"$52.00"
+    }
+]`;
